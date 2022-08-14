0ccd2fa003e9039cc04d9edf8837ebb307cefd10
Katsiaryna26 <mary260188@mail.ru>
2022-08-14 08:13:07 +0300

    feat:implement number guessing function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor(min,max) {
+        this.min=min;
+        this.max=max;  
     }
 
-    guess() {
+    setRange(min, max) {
+     this.min=min;
+     this.max=max;
+      
+    }
+
+    guess() { 
+        this.mid = Math.round((this.max-this.min)/2 + this.min);  
+        
+    return this.mid;
 
     }
 
     lower() {
-
+this.max=this.mid;
     }
 
     greater() {
-
+this.min=this.mid;
     }
 }
 
